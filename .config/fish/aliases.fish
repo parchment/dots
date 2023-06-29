a0e50abe70050ad666d0cf6486c5c510b73baecb
Victor Parchment <services@vparchment.com>
2023-06-29 22:58:44 +0100

    initial commit

---
+++ .config/fish/aliases.fish
@@ -0,0 +1,15 @@
+# Navigation
+alias r='ranger'
+
+alias dk='cd ~/Desktop'
+alias dc='cd ~/Documents'
+alias dl='cd ~/Downloads'
+
+alias sqlstart='brew services start mysql'
+alias sqlstop='brew services top mysql'
+alias n4jstart='brew services start neo4j'
+alias n4jstop='brew services stop neo4j'
+alias pgstart='brew services run postgresql'
+alias pgstop='brew services stop postgresql'
+
+alias vim='nvim'
