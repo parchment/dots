6cbe3f4cfa67fa637a91a5f112c853870af473ab
Victor Parchment <services@vparchment.com>
2024-10-03 17:12:05 +0100

    Swap Hyper for Wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,77 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.window_padding = {
+  left = 20,
+  right = 20,
+  top = 10,
+  bottom = 10,
+}
+
+config.font = wezterm.font 'Hack'
+config.font_size = 13.0
+font_antialias = "Subpixel"
+config.line_height = 1.1
+
+config.colors = {
+  foreground = '#FFFFFF',
+  background = '#282529',
+  ansi = {
+      '#000000',
+      '#E06682',
+      '#A9DC76',
+      '#FFD866',
+      '#78DCE8',
+      '#EE9D70',
+      '#AB9DF2',
+      '#C7C7C7',
+  },
+  brights = {
+      '#C7C7C7',
+      '#FF6188',
+      '#A9DC76',
+      '#FFD866',
+      '#78DCE8',
+      '#FC9867',
+      '#AB9DF2',
+      '#FFFFFF',
+  },
+  cursor_bg = '#FFD866',
+  cursor_fg = 'black',
+  cursor_border = '#FFD866',
+
+  tab_bar = {
+      active_tab = {
+        bg_color = '#282529',
+        fg_color = '#ffffff',
+        intensity = 'Normal',
+        underline = 'None',
+        italic = false,
+        strikethrough = false,
+      },
+      inactive_tab_edge = '#282529',
+      inactive_tab = {
+          bg_color = '#282529',
+          fg_color = '#808080',
+      },
+      inactive_tab_hover = {
+          bg_color = '#282529',
+          fg_color = '#A9DC76',
+          italic = true,
+      },
+    },
+ }
+config.use_fancy_tab_bar = true
+config.show_new_tab_button_in_tab_bar = false
+ config.window_frame = {
+   font = wezterm.font { family = 'Hack', weight = 'Bold' },
+   font_size = 13.0,
+   active_titlebar_bg = '#282529',
+   inactive_titlebar_bg = '#282529',
+ }
+
+return config
